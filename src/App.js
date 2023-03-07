ac9eec2d923d194f073e87230b7f7a1565ff3700
Max Winderbaum <maxwinderbaum@gmail.com>
2023-03-07 07:21:07 -0800

    Working really well

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react"
-import "./App.css"
+import React, { Component } from "react";
+import "./App.css";
 
 import formatHighlight from "json-format-highlight";
 import jsonParse from "json-parse-even-better-errors";
+import prettier from "prettier-standalone";
 
 const customColorOptions = {
   keyColor: "black",
@@ -13,31 +14,66 @@ const customColorOptions = {
   nullColor: "cornflowerblue",
 };
 
+const strategies = [
+  {
+    name: "JSON.parse",
+    func: (str) => jsonParse(str),
+  },
+  {
+    name: "eval",
+    func: (str) => {
+      eval(`window.json = ${str}`); // eslint-disable-line no-eval
+      return window.json;
+    },
+  },
+  {
+    name: "JSON.parse with formatJson",
+    func: (str) => JSON.parse(formatJson(str)),
+  },
+  {
+    name: "prettier",
+    func: (str) => prettier.format(`log = ${str}`, { parser: "babel" }),
+  },
+];
+
 function prettifyJson(input) {
   let output = "";
   let brackets = getBrackets(input);
-  console.log(input)
+  console.log(input);
   console.log(brackets);
   for (let str of brackets) {
     console.log("Trying string", str);
-    try {
-      let jsonObj = jsonParse(str);
-      output += formatHighlight(jsonObj, customColorOptions) + "\n";
-      console.log("Success")
-    } catch (e) {
-      console.log("Trying formatting the string like this:", formatJson(str), e);
-      try {
-        eval(`window.json = ${str}`)
+    let success = false;
 
-        output += formatHighlight(window.json, customColorOptions) + "\n";
-        console.log("Success")
+    for (let strategy of strategies) {
+      try {
+        let jsonObj = strategy.func(str);
+        const formattedString = formatHighlight(jsonObj, customColorOptions);
+        output += formattedString + "\n";
+        console.log("Success with strategy", strategy.name);
+        console.log("formatted string", formattedString);
+        console.log("string", str);
+        console.log("");
+        success = true;
+        break;
       } catch (e) {
-        console.log("Outputting the regular string", e, "\n")
-        output += str + "\n";
+        console.log("Failed with strategy", strategy.name);
+        console.log(e);
       }
     }
+    if (!success) {
+      console.log("Outputting the regular string", "\n");
+      output += formatRegularString(str) + "\n";
+    }
+    output += "<hr>";
   }
   return formatHtml(output);
+}
+
+function formatRegularString(str) {
+  return str
+    .trimLeft()
+    .replace(/\n +/g, "\n");
 }
 
 function formatJson(str) {
@@ -56,13 +92,35 @@ function formatHtml(str) {
 
 function getBrackets(str) {
   const result = []; // create an empty array to store the results
-  const regex = /(\[.*?\])|({.*?})|([^\[{}\]]+)/g; // define a regular expression that matches any text between [], or {}, or any text that is not a bracket
-  let match; // create a variable to store each match
-  while ((match = regex.exec(str))) {
-    // loop through all matches in the string
-    result.push(match[0]); // push each match to the result array
+  const openingBrackets = ["[", "{"]; // define an array of opening brackets
+  const closingBrackets = ["]", "}"]; // define an array of closing brackets
+
+  let numOpenBrackets = 0; // create a variable to store the number of open brackets
+  let currentString = ""; // create a variable to store the current string
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i]; // store the current character in a variable
+    if (openingBrackets.includes(char)) {
+      if (numOpenBrackets === 0 && currentString.length > 1) {
+        // if there are no open brackets
+        result.push(currentString);
+        currentString = ""; // reset the current string
+      }
+      numOpenBrackets++; // increment the number of open brackets
+      currentString += str[i]; // add the current character to the current string
+    } else if (closingBrackets.includes(char) && numOpenBrackets > 0) {
+      numOpenBrackets--; // decrement the number of open brackets
+      currentString += str[i]; // add the current character to the current string
+      if (numOpenBrackets === 0) {
+        // if there are no open brackets
+        result.push(currentString); // push the current string to the result array
+        currentString = ""; // reset the current string
+      }
+    } else {
+      currentString += str[i]; // add the current character to the current string
+    }
   }
-  return result; // return the result array
+  return result;
 }
 
 class App extends Component {
@@ -85,8 +143,8 @@ class App extends Component {
   }
 
   render() {
-    return <div className="App" ref={this.divRef}></div>;
+    return <pre className="App" ref={this.divRef}></pre>;
   }
 }
 
-export default App
+export default App;
